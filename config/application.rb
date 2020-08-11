a5c2e4e011753b0a78fbdbc74819ace89285cb57
github kinop1987 <kinop1987@gmail.com>
2020-08-12 03:53:13 +0900

    routing_home_index

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module ChefsOfficeWorking
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -33,3 +38,4 @@ module ChefsOfficeWorking
     config.generators.system_tests = nil
   end
 end
+
