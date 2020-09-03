87934780a13222910390de76cf96c983fcbd98a4
github kinop1987 <kinop1987@gmail.com>
2020-09-03 19:38:22 +0900

    create_voucher_features
    ###What
    伝票作成機能に必要なモデルの作成
    ###WHY
    伝票を作成し、データベースに保存するため

---
+++ db/migrate/20200903100052_create_vouchers.rb
@@ -0,0 +1,11 @@
+class CreateVouchers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :vouchers do |t|
+      t.integer :total_price, null:false
+      t.date :delivery_date, null:false
+      t.references :order, null:false, foreign_key: true
+      t.references :supplier, null:false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20200903101340_add_details_to_vouchers.rb
@@ -0,0 +1,6 @@
+class AddDetailsToVouchers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :vouchers, :comment, :string
+    add_column :vouchers, :confirm, :boolean, null:false, default:false
+  end
+end
